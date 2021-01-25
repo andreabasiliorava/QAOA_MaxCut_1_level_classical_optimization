ff75add61aa5ff3bafabc339ec889b98b9cd33cd
Andrea Basilio Rava <andreabasilio.rava@studio.unibo.it>
2021-01-25 15:42:26 +0100

    created testing.py and QAOA.py

---
+++ QAOA.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 15:12:05 2021
+
+@author: AndreaB.Rava
+"""
+
+
+
+#Define C
+def evaluate_C (z_str):
+    C = 0
+    z = list(z_str)
+    for edge in edges:
+        C += ((int(z[edge[0]])-int(z[edge[1]])**2)
+    return C
