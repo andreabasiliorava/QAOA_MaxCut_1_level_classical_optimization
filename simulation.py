fa7624a2062a4b7fad62b663216fdbcf20c6165e
Andrea Basilio Rava <andreabasilio.rava@studio.unibo.it>
2021-01-28 16:47:15 +0100

    edited simaluation.py

--- simulation.py
+++ simulation.py
@@ -6,6 +6,7 @@ Created on Mon Jan 25 13:09:06 2021
 """
 
 import qaoa
+import qucompsys as qucs
 import numpy as np
 import matplotlib.pyplot as plt
 import qutip as qu
@@ -15,10 +16,12 @@ import configparser
 
 #take information of the graph
 config = configparser.ConfigParser()
-config.read('butterfly.txt')
+config.read('graphs.txt')
 
-str_n_vertices = config.get('settings', 'n_vertices')
-str_edges = config.get('settings', 'edges')
+#str_graph = input ("telle me the kind of graph",)
+str_graph = 'square'
+str_n_vertices = config.get(str_graph, 'n_vertices')
+str_edges = config.get(str_graph, 'edges')
 
 n_vertices = int(str_n_vertices)
 n_qubits = n_vertices
@@ -32,9 +35,16 @@ n_levels = 1
 
 #pick intial parameters 
 init_params = qaoa.initial_params(n_levels)
+gammas = init_params[0]
+betas = init_params[1]
 
 # initial state |s>:
 init_state = qaoa.initial_state(n_qubits)
+
+# define MaxCut hamiltonian operators
+mix_ham = qaoa.mix_hamilt(n_qubits)
+prob_ham = qaoa.prob_hamilt(n_vertices, edges)
+
 
 
 
