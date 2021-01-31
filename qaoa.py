1dd79399879412f37c422f37b18c2988659ca89d
Andrea Basilio Rava <andreabasilio.rava@studio.unibo.it>
2021-01-31 18:21:41 +0100

    edited qaoa.py

--- qaoa.py
+++ qaoa.py
@@ -5,47 +5,54 @@ Created on Tue Jan 25 10:14:48 2021
 @author: AndreaB.Rava
 """
 
-import numpy as np
 import qutip as qu
 import qucompsys as qucs
 
 
-def evaluate_obj(z_str, edges):
-    """This method evaluates the object function of the MaxCut problem
+def evaluate_cost_fun(z_str, edges):
+    """
+    This method evaluates the object function of the MaxCut problem
+
     Parameters
-        z_str : input bit string
-        edges : edges of the graph
+    ----------
+    z_str : string
+        input bit string.
+    edges : list of tuples
+        edges of the graph.
+
     Returns
-        the integer value the object function"""
-    obj = 0
+    -------
+    cost_fun : int
+        the integer value the object function.
+    """
+
+    cost_fun = 0
     z_list = list(z_str)
     for edge in edges:
-        obj += (int(z_list[edge[0]])-int(z_list[edge[1]]))**2
-    return obj
-
-
-def initial_params(n_levels):
-    """This method generates randomly the intial parameters near zero\n
-    Parameters:\n
-        n_levels: choosen levels of the QAOA algorithm\n
-    Returns:\n
-        an array with shape 2*n_levels (gammas and betas)\n
-    Raise:\n
-        ValueError if number of levels is less than 1"""
-    if n_levels < 1:
-        raise ValueError('number of levels must be > 0, but is {}'.format(n_levels))
-    init_params = 0.01*np.random.rand(2, n_levels)
-    return init_params
+        cost_fun += (int(z_list[edge[0]])-int(z_list[edge[1]]))**2
+    return cost_fun
 
 
 def initial_state(n_qubits):
-    """This method initialize the initial state\n
-    Parameters:\n
-        n_qubits: number of qubits the states is composed of\n
-    Returns:\n
-        an object of the Qobj class defined in qutip, tensor of n-qubits\n
-    Raise:\n
-        ValueError if number of qubits is less than 1"""
+    """
+    This method initialize the initial state\n
+
+    Parameters
+    ----------
+    n_qubits : int
+        number of qubits the states is composed of.
+
+    Raises
+    ------
+    ValueError
+        if number of qubits is less than 1.
+
+    Returns
+    -------
+    init_state : an object of the Qobj class defined in qutip, tensor of n-qubits
+        initial state, tensor of n qubits.
+
+    """
     if n_qubits < 1:
         raise ValueError('number of qubits must be > 0, but is {}'.format(n_qubits))
     list_s = []
@@ -58,14 +65,26 @@ def initial_state(n_qubits):
 
 
 def mix_hamilt(n_qubits):
-    """This method generates a tensor that apply the mixing hamiltonian of the
-        MaxCut problem on a state of n-qubits\n
-    Parameters:\n
-        n_qubits: number of qubits the states is composed of\n
-    Returns:\n
-        a tensor that apply the mixing hamiltonian to a n-qubits state\n
-    Raise:\n
-        ValueError if number of qubits is less than 2"""
+    """
+    This method generates a tensor that apply the mixing hamiltonian of the
+        MaxCut problem on a state of n-qubits
+
+    Parameters
+    ----------
+    n_qubits : int
+        number of qubits the states is composed of.
+
+    Raises
+    ------
+    ValueError
+        if number of qubits is less than 2.
+
+    Returns
+    -------
+    tensor(Qobj)
+        a tensor that apply the mixing hamiltonian to a n-qubits state.
+
+    """
     if n_qubits < 2:
         raise ValueError('number of qubits must be > 1, but is {}'.format(n_qubits))
     list_n_sigmax = []
@@ -75,15 +94,28 @@ def mix_hamilt(n_qubits):
 
 
 def prob_hamilt(n_qubits, edges):
-    """This method generates a tensor that apply the problem hamiltonian of the
-        MaxCut problem on a state of n-qubits\n
-    Parameters:\n
-        vertices: number of vertices of the graph
-        edges: list of tuples corresponding to the edges of the graph\n
-    Returns:\n
-        a tensor that apply the problem hamiltonian to a n-qubits state\n
-    Raise:\n
-        ValueError if number of qubits is less than 2"""
+    """
+    This method generates a tensor that apply the problem hamiltonian of the
+        MaxCut problem on a state of n-qubits
+
+    Parameters
+    ----------
+    n_qubits : int
+        number of qubits the states is composed of.
+    edges : list of tuples
+        edges of the graph.
+
+    Raises
+    ------
+    ValueError
+        if number of qubits is less than 2..
+
+    Returns
+    -------
+    tensor(Qobj)
+        a tensor that apply the mixing hamiltonian to a n-qubits state.
+
+    """
     if n_qubits < 2:
         raise ValueError('number of qubits must be > 1, but is {}'.format(n_qubits))
     list_double_sigmaz = []
@@ -97,19 +129,35 @@ def prob_hamilt(n_qubits, edges):
 def evolution_operator(n_qubits, edges, gammas, betas):
     """
     This method generates a tensor that apply the evolution operator U of the
-        MaxCut problem on a state of n-qubits\n
-    Parameters:\n
-        n_qubits: number of qubits is the n-qubits state among this operators acts on\n
-        edges: edges of the graph of the MaxCut that define the problem hamiltonian
-        gammas: gamma parameters of the MaxCut\n
-        betas: betas parameters of the MaxCut\n
-    Returns:\n
-        a tensor that apply the evolution operator to a n-qubits state\n
-    Raise:\n
-        ValueError if number of gammas is less than 1\n
-        ValueError if number of betas is less than 1\n
-        ValueError if number of betas is different than number of gammas\n
-        ValueError if number of qubits is less than 2      
+    MaxCut problem on a state of n-qubits
+
+    Parameters
+    ----------
+    n_qubits : int
+        n_qubits: number of qubits is the n-qubits state among this operators acts on.
+    edges : list of tuples
+        edges of the graph.
+    gammas : mappable type
+        gammas parameters of the MaxCut.
+    betas : mappable type
+        betas parameters of the MaxCut.
+
+    Raises
+    ------
+    ValueError
+        if number of gammas is less than 1
+    ValueError
+        if number of betas is less than 1
+    ValueError
+        if number of betas is different than number of gammas
+    ValueError
+        if number of qubits is less than 2
+
+    Returns
+    -------
+    evol_oper : tensor (Qobj)
+        tensor representing the evolution operator.
+
     """
     if len(gammas) < 1:
         raise ValueError('number of gammas must be > 0, but is {}'.format(len(gammas)))
