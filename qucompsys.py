379a0fe23436a532da56a70d1697def5e9dd3d65
Andrea Basilio Rava <andreabasilio.rava@studio.unibo.it>
2021-01-31 18:22:34 +0100

    edited qucompsys.py

--- qucompsys.py
+++ qucompsys.py
@@ -8,13 +8,25 @@ import numpy as np
 import qutip as qu
 
 def n_rand_qubits(n_qubits):
-    """This method generates a list of random n qubits\n
-    Parameters:\n
-        n_qubits: number of qubits\n
-    Returns:\n
-        a list of n qubits\n
-    Raise:\n
-        ValueError if number of qubits is less than 1"""
+    """
+    This method generates a list of random n qubits
+
+    Parameters
+    ----------
+    n_qubits : int
+        number of qubits.
+
+    Raises
+    ------
+    ValueError
+        if number of qubits is less than 1.
+
+    Returns
+    -------
+    list_n_rand_qubits : list
+        a list of n qubits.
+
+    """
     if n_qubits < 1:
         raise ValueError('number of qubits must be > 0, but is {}'.format(n_qubits))
     list_n_rand_qubits = []
@@ -30,30 +42,56 @@ def n_rand_qubits(n_qubits):
 
 
 def n_qeye(n_qubits):
-    """This method generates a tensor that apply identity on a state\n
-       of n-qubits
-    Parameters:\n
-        n_qubits: number of qubits the states is composed of\n
-    Returns:\n
-        a tensor that apply the identity to a n-qubits state\n
-    Raise:\n
-        ValueError if number of qubits is less than 1"""
+    """
+    This method generates a tensor that apply identity on a state
+    of n-qubits
+
+    Parameters
+    ----------
+    n_qubits : int
+        number of qubits the state is composed of.
+
+    Raises
+    ------
+    ValueError
+        if number of qubits is less than 1.
+
+    Returns
+    -------
+    tensor (Qobj)
+        a tensor that apply the identity to a n-qubits state.
+
+    """
     if n_qubits < 1:
         raise ValueError('number of qubits must be > 0, but is {}'.format(n_qubits))
     return qu.tensor([qu.qeye(2)]*n_qubits)
 
 
 def n_sigmax(n_qubits, qubit_pos):
-    """This method generates a tensor(Qobj) wich perform a single-qubit sigmax operation
-       on a state of n-qubits\n
-    Parameters:\n
-        n_vertices: number of qubits the states is composed of\n
-        qubit_pos: qubit on which the sigmax operator acts (position starts at '0')\n
-    Returns:\n
-        a tensor that apply sigmax on the nth-qubit of a n-qubits state\n
-    Raise:\n
-        ValueError if number of qubits is less than 2\n
-        ValueError if qubit position is < 0 or > n_qubits-1 """
+    """
+    This method generates a tensor(Qobj) wich perform a single-qubit sigmax operation
+    on a state of n-qubits
+
+    Parameters
+    ----------
+    n_qubits : TYPE
+        number of qubits the states is composed of.
+    qubit_pos : TYPE
+        qubit on which the sigmax operator acts (position starts at '0').
+
+    Raises
+    ------
+    ValueError
+        if number of qubits is less than 2
+    ValueError
+        if qubit position is < 0 or > n_qubits-1
+
+    Returns
+    -------
+    n_sigmax: tensor (Qobj)
+        a tensor that apply sigmax on the nth-qubit of a n-qubits state.
+
+    """
     if n_qubits < 1:
         raise ValueError('number of vertices must be > 0, but is {}'.format(n_qubits))
     if qubit_pos < 0 or qubit_pos > n_qubits-1:
@@ -65,16 +103,30 @@ def n_sigmax(n_qubits, qubit_pos):
 
 
 def n_sigmay(n_qubits, qubit_pos):
-    """This method generates a tensor(Qobj) wich perform a single-qubit sigmay operation
-       on a state of n-qubits\n
-    Parameters:\n
-        n_vertices: number of qubits the states is composed of\n
-        qubit_pos: qubit on which the sigmay operator acts (position starts at '0')\n
-    Returns:\n
-        a tensor that apply sigmay on the nth-qubit of a n-qubits state\n
-    Raise:\n
-        ValueError if number of qubits is less than 2\n
-        ValueError if qubit position is < 0 or > n_qubits-1 """
+    """
+    This method generates a tensor(Qobj) wich perform a single-qubit sigmay operation
+    on a state of n-qubits
+
+    Parameters
+    ----------
+    n_qubits : TYPE
+        number of qubits the states is composed of.
+    qubit_pos : TYPE
+        qubit on which the sigmax operator acts (position starts at '0').
+
+    Raises
+    ------
+    ValueError
+        if number of qubits is less than 2
+    ValueError
+        if qubit position is < 0 or > n_qubits-1
+
+    Returns
+    -------
+    n_sigmaz: tensor (Qobj)
+        a tensor that apply sigmay on the nth-qubit of a n-qubits state.
+
+    """
     if n_qubits < 1:
         raise ValueError('number of vertices must be > 0, but is {}'.format(n_qubits))
     if qubit_pos < 0 or qubit_pos > n_qubits-1:
@@ -86,16 +138,30 @@ def n_sigmay(n_qubits, qubit_pos):
 
 
 def n_sigmaz(n_qubits, qubit_pos):
-    """This method generates a tensor(Qobj) wich perform a single-qubit sigmaz operation
-       on a state of n-qubits\n
-    Parameters:\n
-        n_vertices: number of qubits the states is composed of\n
-        qubit_pos: qubit on which the sigmaz operator acts (position starts at '0')\n
-    Returns:\n
-        a tensor that apply sigmaz on the nth-qubit of a n-qubits state\n
-    Raise:\n
-        ValueError if number of qubits is less than 2\n
-        ValueError if qubit position is < 0 or > n_qubits-1 """
+    """
+    This method generates a tensor(Qobj) wich perform a single-qubit sigmaz operation
+    on a state of n-qubits
+
+    Parameters
+    ----------
+    n_qubits : TYPE
+        number of qubits the states is composed of.
+    qubit_pos : TYPE
+        qubit on which the sigmax operator acts (position starts at '0').
+
+    Raises
+    ------
+    ValueError
+        if number of qubits is less than 2
+    ValueError
+        if qubit position is < 0 or > n_qubits-1
+
+    Returns
+    -------
+    n_sigmaz: tensor (Qobj)
+        a tensor that apply sigmaz on the nth-qubit of a n-qubits state.
+
+    """
     if n_qubits < 1:
         raise ValueError('number of vertices must be > 0, but is {}'.format(n_qubits))
     if qubit_pos < 0 or qubit_pos > n_qubits-1:
@@ -104,45 +170,3 @@ def n_sigmaz(n_qubits, qubit_pos):
     for i in range(n_qubits):
         list_n_sigmaz.append(qu.tensor([qu.qeye(2)]*i+[qu.sigmaz()]+[qu.qeye(2)]*(n_qubits-i-1)))
     return list_n_sigmaz[qubit_pos]
-
-
-def n_proj0(n_qubits, qubit_pos):
-    """This method generates a tensor(Qobj) wich perform a single-qubit projection
-        operation on basis state |0> on a state of n-qubits\n
-    Parameters:\n
-        n_vertices: number of qubits the states is composed of\n
-        qubit_pos: qubit on which the projector operator acts (position starts at '0')\n
-    Returns:\n
-        a tensor that apply the projector on the nth-qubit of a n-qubits state\n
-    Raise:\n
-        ValueError if number of qubits is less than 2\n
-        ValueError if qubit position is < 0 or > n_qubits-1 """
-    if n_qubits < 1:
-        raise ValueError('number of vertices must be > 0, but is {}'.format(n_qubits))
-    if qubit_pos < 0 or qubit_pos > n_qubits-1:
-        raise ValueError('qubit position must be > 0 or < n_qubits-1, but is {}'.format(qubit_pos))
-    list_n_proj0 = []
-    for i in range(n_qubits):
-        list_n_proj0.append(qu.tensor([qu.qeye(2)]*i+[qu.ket('0').proj()]+[qu.qeye(2)]*(n_qubits-i-1)))
-    return list_n_proj0[qubit_pos]
-
-
-def n_proj1(n_qubits, qubit_pos):
-    """This method generates a tensor(Qobj) wich perform a single-qubit projection
-        operation on basis state |1> on a state of n-qubits\n
-    Parameters:\n
-        n_vertices: number of qubits the states is composed of\n
-        qubit_pos: qubit on which the projector operator acts (position starts at '0')\n
-    Returns:\n
-        a tensor that apply the projector on the nth-qubit of a n-qubits state\n
-    Raise:\n
-        ValueError if number of qubits is less than 2\n
-        ValueError if qubit position is < 0 or > n_qubits-1 """
-    if n_qubits < 1:
-        raise ValueError('number of vertices must be > 0, but is {}'.format(n_qubits))
-    if qubit_pos < 0 or qubit_pos > n_qubits-1:
-        raise ValueError('qubit position must be > 0 or < n_qubits-1, but is {}'.format(qubit_pos))
-    list_n_proj1 = []
-    for i in range(n_qubits):
-        list_n_proj1.append(qu.tensor([qu.qeye(2)]*i+[qu.ket('1').proj()]+[qu.qeye(2)]*(n_qubits-i-1)))
-    return list_n_proj1[qubit_pos]
