47fe6b006863356550a6405b397cd1036f14cbec
Andrea Basilio Rava <andreabasilio.rava@studio.unibo.it>
2021-02-10 16:56:33 +0100

    edited simulation.py

--- simulation.py
+++ simulation.py
@@ -5,15 +5,13 @@ Created on Mon Jan 25 13:09:06 2021
 @author: AndreaB.Rava
 """
 
-import scipy
 import qutip as qu
 import qaoa
-import graphs as gr
 import qucompsys as qucs
 import numpy as np
-import matplotlib.pyplot as plt
 import configparser
 import networkx as nx
+import sys
 
 #main part of the code
 
@@ -34,7 +32,7 @@ graph.add_edges_from(edges)
 #METHOD 2: take information from a file
 config = configparser.ConfigParser()
 config.read(sys.argv[1])
-str_graph = sys.arv[2]
+str_graph = sys.argv[2]
 str_N_NODES = config.get(str_graph, 'N_NODES')
 str_edges = config.get(str_graph, 'edges')
 N_NODES = int(str_N_NODES)
@@ -47,8 +45,7 @@ graph = nx.Graph()
 graph.add_nodes_from(nodes)
 graph.add_edges_from(edges)
 
-destination1 = config.get('paths','my_graph')
-destination2 = config.get('paths','my_prob_dist')
+destination1 = config.get('paths','my_prob_dist')
 #"""
 
 """
@@ -60,11 +57,6 @@ graph = gr.random_graph(N_NODES)
 edges = list(graph.edges)
 """
 
-#Plot the graph
-colors = ['r' for node in graph.nodes()]
-pos = nx.circular_layout(graph)
-graph_drawing = nx.draw_networkx(graph, node_color=colors, node_size=200, alpha=1, pos=pos, with_labels=True)
-plt.show()
 
 #STEP 2: find optimal parameters
 # Grid search for the maximizing variables
@@ -72,11 +64,13 @@ step_size = 0.01
 a_gamma         = np.arange(0.0, np.pi, step_size)
 a_beta          = np.arange(0.0, np.pi/2, step_size)
 a_gamma, a_beta = np.meshgrid(a_gamma, a_beta, indexing='xy')
-grid_f_1 = analitic_f_1(a_gamma, a_beta, graph, edges)
+grid_f_1 = qaoa.analitical_f_1(a_gamma, a_beta, graph, edges)
 result = np.where(grid_f_1 == np.amax(grid_f_1))
 a      = list(zip(result[0],result[1]))[0]
 optimal_gamma   = a[1]*step_size
 optimal_beta  = a[0]*step_size
+print(optimal_gamma)
+print(optimal_beta)
 
 #STEP 3: obtain final state with solutions
 # initial state (as density matrix):
@@ -90,12 +84,4 @@ dm_fin_state = qu.ket2dm(fin_state)
 #probability distributions of configurations in final state
 prob_dist_fin_state = qucs.comp_basis_prob_dist(fin_state)
 
-np.save(destination1, graph)
-np.save(destination2, prob_dist_fin_state)
-
-
-plt.figure(figsize = (2**N_QUBITS/2.5,20))
-plt.xticks(rotation=45)
-xticks = range(0,2**(N_QUBITS-1))
-xtick_labels = list(map(lambda x: format(x, "0"+str(N_QUBITS)+'b'), xticks))
-plt.bar(xtick_labels,prob_dist_fin_state[:2**(N_QUBITS-1)],width = 0.5)
+np.save(destination1, prob_dist_fin_state)
